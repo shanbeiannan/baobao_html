20923fe5780a92a11b0958c83b75000c16016285
shanbeiannan <1761991696@qq.com>
2020-01-07 19:27:00 +0800

    弹出层

--- pages/order/shanmenshouhuo.js
+++ pages/order/shanmenshouhuo.js
@@ -5,9 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    istrue:false
   },
-
+  openDialog: function () {
+    this.setData({
+      istrue: true
+    })
+  },
+  closeDialog: function () {
+    this.setData({
+      istrue: false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
