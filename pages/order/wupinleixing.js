c0268379096a0c375286bbfac9a5d72f92bcc290
shanbeiannan <1761991696@qq.com>
2020-01-15 11:11:04 +0800

    样式调整

--- pages/order/wupinleixing.js
+++ pages/order/wupinleixing.js
@@ -4,8 +4,8 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {    
-    isShowChild:0,
+  data: {
+    isShowChild: 0,
   },
 
   /**
@@ -13,28 +13,77 @@ Page({
    */
   onLoad: function (options) {
     var $systemHeight = 0;
-      wx.getSystemInfo({
-        complete: (res) => {},
-        fail: (res) => {},
-        success: (result) => {
-          console.log(result.windowHeight)
-          $systemHeight = result.windowHeight;
-          this.setData({
-            systemHeight:$systemHeight,
-            showSelectHeight:$systemHeight*0.3,
-            showProductHeight:$systemHeight*0.7
-          })
-        },
+    wx.getSystemInfo({
+      complete: (res) => {},
+      fail: (res) => {},
+      success: (result) => {
+
+        $systemHeight = result.windowHeight;
+        this.setData({
+          systemHeight: $systemHeight,
+          showSelectHeight: $systemHeight * 0.3,
+          showProductHeight: $systemHeight * 0.7
+        })
+      },
+    })
+
+
+    let {
+      keys,
+      values
+    } = {
+      keys: ["product1", "product2", "product3"],
+      values: ["0", "1", "5"]
+    };
+    keys.forEach((item, index) => {
+      this.setData({
+        [item]: values[index]
       })
+    });
+
+    console.log(this.data.product3);
+
+
+
+    var keyArr = [];
+    var valueArr = [];
+
+
+    // var productList = that.data.dataOrderListSelect;
+    // for (var i = 0; i < productList.length; i++) {
+    //   var keyName = 'product' + productList.productid;
+    //   keyArr.push(keyName);
+    //   var valueName = false;
+    //   if (productList.sum > 0) {
+    //     valueName = true;
+    //   }
+    //   valueArr.push(valueName);
+    // }
+    // console.log(valueArr);
+    // let {
+    //   keys,
+    //   values
+    // } = {
+    //   keys: keyArr,
+    //   values: valueArr
+    // }
+    // keys.forEach((item, index) => {
+    //   this.setData({
+    //     [item]: values[index]
+    //   })
+    // });
+
+
+
   },
-  showClassChild:function(e){
+  showClassChild: function (e) {
     var $mainClass = e.currentTarget.dataset.index;
     this.setData({
-      isShowChild:$mainClass
-    }) 
+      isShowChild: $mainClass
+    })
   },
-  selectProduct:function(e){
-    
+  selectProduct: function (e) {
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
